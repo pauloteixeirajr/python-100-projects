e25a8f416f2268e80b59ac08504dbe85f9abbfec
Paulo Teixeira <pauloteixeirajr@outlook.com>
2021-04-01 22:13:46 +0100

    day1: the python input function

--- day1/main.py
+++ day1/main.py
@@ -1,9 +1,11 @@
+# The Python Input function
+print('Hello ' + input('What is your name? '))
+
 # String Manipulation
 print('Hello world!\nI am in a new line')
 print('Hello' + ' ' + 'Paulo')
 
 # Printing to the console
-
 print('Hello world!')
 print('Day 1 - Python Print Function')
 print('The function is declared like this:')
