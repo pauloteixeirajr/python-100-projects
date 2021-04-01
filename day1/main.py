ba85d81dc36e575bed6f3add2193a514a4bfc2ee
Paulo Teixeira <pauloteixeirajr@outlook.com>
2021-04-01 22:02:03 +0100

    day1: string manipulation

--- day1/main.py
+++ day1/main.py
@@ -1,3 +1,9 @@
+# String Manipulation
+print('Hello world!\nI am in a new line')
+print('Hello' + ' ' + 'Paulo')
+
+# Printing to the console
+
 print('Hello world!')
 print('Day 1 - Python Print Function')
 print('The function is declared like this:')
