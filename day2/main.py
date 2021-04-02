3ff53269df3572b4270b810e8e0e335d0ee9c9ae
Paulo Teixeira <pauloteixeirajr@outlook.com>
2021-04-02 21:29:36 +0100

    day2: python primitive data types

---
+++ day2/main.py
@@ -0,0 +1,16 @@
+# Python Primitive Data Types
+# Strings
+print("Hello"[0])  # = H
+print("Hello"[4])  # = o
+print("123" + "456")  # 123456
+
+# Integer (whole numbers)
+print(123 + 456)  # 579
+print(123_456_789)  # 123456789 (py removes the underscores)
+
+# Floats
+print(3.14159)
+
+# Booleans
+print(True)
+print(False)
