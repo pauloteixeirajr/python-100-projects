7690e51c63b22c3dea3c6f452916951fd9bb24ed
Paulo Teixeira <pauloteixeirajr@outlook.com>
2021-04-02 21:38:58 +0100

    day2: type error, checking and conversion

--- day2/main.py
+++ day2/main.py
@@ -1,3 +1,15 @@
+# Type Error, Type Checking and Type Conversion
+print(type('string'))  # <class 'str'>
+print(type(12345679))  # <class 'int'>
+print(type(12345.67))  # <class 'float'>
+print(type(True))  # <class 'bool'>
+print(type(False))  # <class 'bool'>
+
+# Converting to avoid Type Errors
+print(str(123) + ' is a nice number')
+print(int("12") + 10)
+print(float(70) + 100.5)
+
 # Python Primitive Data Types
 # Strings
 print("Hello"[0])  # = H
