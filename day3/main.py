778f0810da77218087a653ff8b32d78cf7de442a
Paulo Teixeira <pauloteixeirajr@outlook.com>
2021-04-06 21:28:23 +0100

    day3: logical operators

--- day3/main.py
+++ day3/main.py
@@ -20,6 +20,8 @@ if height >= 120:
     elif age <= 18:
         bill = 7
         print("Youth tickets are $7.")
+    elif age >= 45 and age <= 55:
+        print("Everything is going to be okay. You get a free ride!")
     else:
         bill = 12
         print("Adult tickets are $12.")
