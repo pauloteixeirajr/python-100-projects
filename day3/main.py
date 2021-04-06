b85a230c09eef4c5d797432a29dc473a555b00ac
Paulo Teixeira <pauloteixeirajr@outlook.com>
2021-04-06 20:24:14 +0100

    day3: control flow with if else and conditional operators

---
+++ day3/main.py
@@ -0,0 +1,16 @@
+# Control flow with if / else and conditional operators
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+# Comparison operators
+# > greater than
+# < less than
+# >= greater than or equal
+# <= less than or equal
+# == equal
+# != not equal
+
+if height >= 120:
+    print("You can ride the rollercoaster!")
+else:
+    print("Sorry, you have to grow taller before you can ride.")
