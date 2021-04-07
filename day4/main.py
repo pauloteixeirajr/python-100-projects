4cdd3e22fcdef94762fdc5b03bbfbf0a304e5225
Paulo Teixeira <pauloteixeirajr@outlook.com>
2021-04-07 19:35:58 +0100

    day4: random module

---
+++ day4/main.py
@@ -0,0 +1,11 @@
+# Random Module
+import random
+
+random_int = random.randint(1, 10)
+# 0 - 1
+random_float = random.random()
+
+print(random_int)
+print(random_float)
+# 0 - 4
+print(random_float * 5)
