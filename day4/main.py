a993d1bda366ddb53f2b2dd4c7da49bb88ce1a21
Paulo Teixeira <pauloteixeirajr@outlook.com>
2021-04-07 19:47:52 +0100

    day4: python lists

--- day4/main.py
+++ day4/main.py
@@ -9,3 +9,14 @@ print(random_int)
 print(random_float)
 # 0 - 4
 print(random_float * 5)
+
+# Python Lists
+states = ["Delaware", "Pennsylvania", "New York"]
+states.append("California")
+states.extend(["Alaska", "Hawaii"])
+
+print(states)
+print(states[0])
+print(states[1])
+print(states[2])
+print(states[-1])
