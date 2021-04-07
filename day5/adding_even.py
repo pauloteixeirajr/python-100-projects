a5020fbedf971620d5125c57b96402f32fd8cb91
Paulo Teixeira <pauloteixeirajr@outlook.com>
2021-04-07 21:55:50 +0100

    day5: adding even numbers

---
+++ day5/adding_even.py
@@ -0,0 +1,7 @@
+# Write a program that calculates the sum of all the even
+# numbers from 1 to 100, including 1 and 100
+total = 0
+for number in range(2, 101, 2):
+    total += number
+
+print(total)
