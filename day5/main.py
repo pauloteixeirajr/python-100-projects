5c29efcf6bc616a52f32b57575aa339364403f87
Paulo Teixeira <pauloteixeirajr@outlook.com>
2021-04-07 21:50:26 +0100

    day5: for loops and the range function

--- day5/main.py
+++ day5/main.py
@@ -4,3 +4,15 @@ fruits = ["Apple", "Peach", "Pear"]
 for fruit in fruits:
     print(fruit)
     print(fruit + " Pie")
+
+# for loops and the range function
+# doesn't print the last number
+for number in range(1, 11):
+    print(number)
+
+# Adding all numbers from 1 to 100
+total = 0
+for number in range(1, 101):
+    total += number
+
+print(total)
