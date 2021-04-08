08dc9690e1b6661d61c1730a09fa8b95185644f8
Paulo Teixeira <pauloteixeirajr@outlook.com>
2021-04-08 20:27:42 +0100

    day6: while loops

--- day6/main.py
+++ day6/main.py
@@ -7,3 +7,10 @@ def my_function():
 my_function()
 
 # https://reeborg.ca/reeborg.html to play around with functions
+
+# While Loops
+number_of_steps = 6
+
+while number_of_steps > 0:
+    print(number_of_steps)
+    number_of_steps -= 1
