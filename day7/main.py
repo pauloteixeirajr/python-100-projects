d52ba7371b991716f898c4f040751c456469d071
Paulo Teixeira <pauloteixeirajr@outlook.com>
2021-04-08 21:57:15 +0100

    day7: picking random words and checking answers

---
+++ day7/main.py
@@ -0,0 +1,18 @@
+# The Hangman Game
+import random
+
+# Step 1
+word_list = ["aardvark", "baboon", "camel"]
+
+# TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+chosen_word = random.choice(random)
+
+# TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+guess = input("Guess a letter: ").lower()
+
+# TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+for letter in chosen_word:
+    if letter == guess:
+        print('Right')
+    else:
+        print('Wrong')
