14d456f6ee782b4748ee0a905b2b326e032d93fd
Paulo Teixeira <pauloteixeirajr@outlook.com>
2021-04-08 22:25:13 +0100

    day7: checking if the player has won

--- day7/main.py
+++ day7/main.py
@@ -1,34 +1,31 @@
 # The Hangman Game
 import random
 
-# Step 2
+# Step 3
 word_list = ["ardvark", "baboon", "camel"]
 
 chosen_word = random.choice(word_list)
 print(chosen_word)
 
-# TODO-1: - Create an empty List called display.
-# For each letter in the chosen_word, add a "_" to 'display'.
-# So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"]
-# with 5 "_" representing each letter to guess.
 display = []
 for _ in chosen_word:
     display.append('_')
 
-guess = input("Guess a letter: ").lower()
+# TODO-1: - Use a while loop to let the user guess again. The loop should only stop once the user
+# has guessed all the letters in the chosen_word and 'display' has no more blanks ("_").
+# Then you can tell the user they've won.
 
-# TODO-2: - Loop through each position in the chosen_word;
-# If the letter at that position matches 'guess' then reveal
-# that letter in the display at that position.
-# e.g. If the user guessed "p" and the chosen word was "apple",
-# then display should be ["_", "p", "p", "_", "_"].
-for position in range(len(chosen_word)):
-    letter = chosen_word[position]
-    if letter == guess:
-        display[position] = letter
+end_of_game = False
 
-# TODO-3: - Print 'display' and you should see the guessed letter
-# in the correct position and every other letter replace with "_".
-# Hint - Don't worry about getting the user to guess the next letter.
-# We'll tackle that in step 3.
-print(display)
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+
+    for position in range(len(chosen_word)):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
+
+    print(display)
+    if '_' not in display:
+        end_of_game = True
+        print("You win!")
