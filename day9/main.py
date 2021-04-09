680feb8a1844ae4a2c1071f6a3847d1843a573ec
Paulo Teixeira <pauloteixeirajr@outlook.com>
2021-04-09 21:06:03 +0100

    day9: the python dictionary deep dive

---
+++ day9/main.py
@@ -0,0 +1,19 @@
+# Dictionaries and Nesting
+programming_dictionary = {
+    "Bug": "An error in a program that prevents the program from running as expected.",
+    "Function": "A piece of code that you can easily call over and over again.",
+}
+
+# Retrieving items from dictionary
+print(programming_dictionary["Bug"])
+print(programming_dictionary["Function"])
+
+# Adding new items to dictionary
+programming_dictionary["Loop"] = "The action of doing something over and over again."
+
+# Edit an item in a dictionary
+programming_dictionary["Loop"] = "A built-in statement that allows you to repeat code in a loop."
+
+# Loop through a dictionary
+for key in programming_dictionary:
+    print(programming_dictionary[key])
