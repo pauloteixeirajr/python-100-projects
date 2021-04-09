4226a31697ee5c0522c549c62294fd15aaf48027
Paulo Teixeira <pauloteixeirajr@outlook.com>
2021-04-09 21:19:14 +0100

    day9: grading program

---
+++ day9/grading_program.py
@@ -0,0 +1,33 @@
+# Write a program that converts students scores to grades.
+# By the end of the program, you should have a new dictionary
+# called student_grades that should contain the student names
+# for keys and their grades for values.
+#
+# Scoring Criteria
+# Scores 91 - 100: Grade = "Outstanding"
+# Scores 81 - 90: Grade = "Exceeds Expectations"
+# Scores 71 - 80: Grade = "Acceptable"
+# Scores 70 or lower: Grade = "Fail"
+
+student_scores = {
+    "Harry": 81,
+    "Ron": 78,
+    "Hermione": 99,
+    "Draco": 74,
+    "Neville": 62
+}
+
+student_grades = {}
+
+for student in student_scores:
+    score = student_scores[student]
+    if score > 90:
+        student_grades[student] = "Outstanding"
+    elif score > 80:
+        student_grades[student] = "Exceeds Expectations"
+    elif score > 70:
+        student_grades[student] = "Acceptable"
+    else:
+        student_grades[student] = "Fail"
+
+print(student_grades)
