957cf26305459a2250b16b10a5701746365cdc04
Paulo Teixeira <pauloteixeirajr@outlook.com>
2021-04-09 17:53:38 +0100

    day8: functions with inputs

---
+++ day8/main.py
@@ -0,0 +1,12 @@
+# Functions with inputs
+# Create a function called greet()
+# Write 3 print statements inside the function
+# Call the greet() function and run your code
+def greet(name):
+    print(f"Hi {name}")
+    print("How do you do?")
+    print("Isn't the weather nice today?")
+
+
+greet("Paulo")
+greet("John")
