4a556dad5f3fe1279579206e7e2ce4f1b802a83a
Paulo Teixeira <pauloteixeirajr@outlook.com>
2021-04-12 21:00:03 +0100

    day10: docstrings

--- day10/main.py
+++ day10/main.py
@@ -1,5 +1,9 @@
 # Function with outputs
 def format_name(f_name, l_name):
+    """ 
+    Take a first and last name and format it to
+    return the title case version of the name.
+    """
     return f'{f_name} {l_name}'.title()
 
 
