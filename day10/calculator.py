70c4fb11a5c3e77e8e2f4b37122afca638ac787f
Paulo Teixeira <pauloteixeirajr@outlook.com>
2021-04-12 22:02:59 +0100

    day10: calculator project

---
+++ day10/calculator.py
@@ -0,0 +1,66 @@
+logo = """
+ _____________________
+|  _________________  |
+| | Pythonista   0. | |  .----------------.  .----------------.  .----------------.  .----------------. 
+| |_________________| | | .--------------. || .--------------. || .--------------. || .--------------. |
+|  ___ ___ ___   ___  | | |     ______   | || |      __      | || |   _____      | || |     ______   | |
+| | 7 | 8 | 9 | | + | | | |   .' ___  |  | || |     /  \     | || |  |_   _|     | || |   .' ___  |  | |
+| |___|___|___| |___| | | |  / .'   \_|  | || |    / /\ \    | || |    | |       | || |  / .'   \_|  | |
+| | 4 | 5 | 6 | | - | | | |  | |         | || |   / ____ \   | || |    | |   _   | || |  | |         | |
+| |___|___|___| |___| | | |  \ `.___.'\  | || | _/ /    \ \_ | || |   _| |__/ |  | || |  \ `.___.'\  | |
+| | 1 | 2 | 3 | | x | | | |   `._____.'  | || ||____|  |____|| || |  |________|  | || |   `._____.'  | |
+| |___|___|___| |___| | | |              | || |              | || |              | || |              | |
+| | . | 0 | = | | / | | | '--------------' || '--------------' || '--------------' || '--------------' |
+| |___|___|___| |___| |  '----------------'  '----------------'  '----------------'  '----------------' 
+|_____________________|
+"""
+
+
+def add(n1, n2):
+    return n1 + n2
+
+
+def subtract(n1, n2):
+    return n1 - n2
+
+
+def multiply(n1, n2):
+    return n1 * n2
+
+
+def divide(n1, n2):
+    return n1 / n2
+
+
+operations = {
+    '+': add,
+    '-': subtract,
+    '*': multiply,
+    '/': divide
+}
+
+
+def calculator():
+    print(logo)
+    num1 = float(input("What's the first number?: "))
+
+    for symbol in operations:
+        print(symbol)
+
+    should_continue = True
+
+    while should_continue:
+
+        operation = input("Pick an operation: ")
+        num2 = float(input("What's the next number?: "))
+        answer = operations[operation](num1, num2)
+
+        print(f"{num1} {operation} {num2} = {answer}")
+        if input("Type 'y' to continue with {answer}, or 'n' to start again: ") == 'y':
+            num1 = answer
+        else:
+            should_continue = False
+            calculator()
+
+
+calculator()
