8d6f24e6ceb9c54c79d4fbafd5ffe111beebb173
Paulo Teixeira <pauloteixeirajr@outlook.com>
2021-04-14 21:28:17 +0100

    day17: the quizbrain and the next_question method

--- intermediate/day17/main.py
+++ intermediate/day17/main.py
@@ -1,8 +1,12 @@
-from question_model import Question
 from data import question_data
+from question_model import Question
+from quiz_brain import QuizBrain
 
 question_bank = []
 for question in question_data:
     text, answer = question["text"], question["answer"]
     new_question = Question(text, answer)
     question_bank.append(new_question)
+
+quiz = QuizBrain(question_bank)
+quiz.next_question()
