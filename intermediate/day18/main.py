9eb46246e61741e7fb4f9687bfbd6bc83e574f93
Paulo Teixeira <pauloteixeirajr@outlook.com>
2021-04-14 22:15:51 +0100

    day18: draw a dashed line

--- intermediate/day18/main.py
+++ intermediate/day18/main.py
@@ -5,9 +5,16 @@ timmy = Turtle()
 timmy.shape("turtle")
 timmy.color("red")
 # Draw a square
-for _ in range(4):
-    timmy.forward(100)
-    timmy.right(90)
+# for _ in range(4):
+#     timmy.forward(100)
+#     timmy.right(90)
+
+# Draw a dashed line
+for i in range(20):
+    timmy.forward(5)
+    timmy.penup()
+    timmy.forward(5)
+    timmy.pendown()
 
 
 screen = Screen()
