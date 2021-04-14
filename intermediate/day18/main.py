37cbc8420710b6e74e59853fe812e34dc052af5e
Paulo Teixeira <pauloteixeirajr@outlook.com>
2021-04-14 22:30:12 +0100

    day18: drawing different shapes

--- intermediate/day18/main.py
+++ intermediate/day18/main.py
@@ -3,19 +3,30 @@ from turtle import Turtle, Screen
 
 timmy = Turtle()
 timmy.shape("turtle")
-timmy.color("red")
 # Draw a square
 # for _ in range(4):
 #     timmy.forward(100)
 #     timmy.right(90)
 
 # Draw a dashed line
-for i in range(20):
-    timmy.forward(5)
-    timmy.penup()
-    timmy.forward(5)
-    timmy.pendown()
+# for i in range(20):
+#     timmy.forward(5)
+#     timmy.penup()
+#     timmy.forward(5)
+#     timmy.pendown()
 
+colors = [
+    "CornflowerBlue", "DarkOrchid", "IndianRed",
+    "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"
+]
+
+# Drawing Different Shapes
+for i in range(3, 11):
+    angle = 360 / i
+    timmy.color(colors[i - 3])
+    for _ in range(i):
+        timmy.forward(100)
+        timmy.right(angle)
 
 screen = Screen()
 screen.exitonclick()
