a0c40707ab50809a17de8afdb6cdf4251226a358
Paulo Teixeira <pauloteixeirajr@outlook.com>
2021-04-14 21:33:27 +0100

    day17: continue showing new questions

--- intermediate/day17/quiz_brain.py
+++ intermediate/day17/quiz_brain.py
@@ -2,8 +2,12 @@ class QuizBrain:
     def __init__(self, question_list):
         self.question_number = 0
         self.question_list = question_list
+        self.size = len(self.question_list)
 
     def next_question(self):
         question = self.question_list[self.question_number]
         self.question_number += 1
         input(f"Q. {self.question_number}: {question.text} (True/False): ")
+
+    def still_has_questions(self):
+        return self.question_number < self.size
