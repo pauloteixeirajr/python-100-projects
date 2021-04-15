c3143f5485c05ee8e96c510e17503888320f3a8e
Paulo Teixeira <pauloteixeirajr@outlook.com>
2021-04-15 23:06:05 +0100

    day21: detect collisions with the wall

--- intermediate/day21/scoreboard.py
+++ intermediate/day21/scoreboard.py
@@ -22,3 +22,7 @@ class Scoreboard(Turtle):
 
     def update_scoreboard(self):
         self.write(f"Score: {self.score}", align=ALIGN, font=FONT)
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER", align=ALIGN, font=FONT)
