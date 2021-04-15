bb1fe8f269109c20f3cc1787ef61b78aa962e4eb
Paulo Teixeira <pauloteixeirajr@outlook.com>
2021-04-15 22:58:41 +0100

    day21: create a scoreboard and keep score

--- intermediate/day21/main.py
+++ intermediate/day21/main.py
@@ -2,6 +2,7 @@
 import time
 from turtle import Screen
 from food import Food
+from scoreboard import Scoreboard
 from snake import Snake
 
 
@@ -13,6 +14,7 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
+scoreboard = Scoreboard()
 
 # Listen for key strokes
 screen.listen()
@@ -35,6 +37,7 @@ while game_is_on:
     # Detect collision with food
     if snake.head.distance(food) < 15:
         food.refresh()
+        scoreboard.increase_score()
 
 
 screen.exitonclick()
---
+++ intermediate/day21/scoreboard.py
@@ -0,0 +1,24 @@
+from turtle import Turtle
+
+ALIGN = "center"
+FONT = ("Courier", 24, "normal")
+
+
+class Scoreboard(Turtle):
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.score = 0
+        self.color("white")
+        self.penup()
+        self.hideturtle()
+        self.goto(x=0, y=270)
+        self.update_scoreboard()
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.write(f"Score: {self.score}", align=ALIGN, font=FONT)
