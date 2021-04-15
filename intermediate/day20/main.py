db966c8784d02befc00ffec2acf5b3aaf1f45dcd
Paulo Teixeira <pauloteixeirajr@outlook.com>
2021-04-15 21:38:55 +0100

    day20: control the snake with keypresses

--- intermediate/day20/main.py
+++ intermediate/day20/main.py
@@ -12,7 +12,19 @@ screen.tracer(0)
 
 snake = Snake()
 
+# Listen for key strokes
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+screen.onkey(snake.up, "w")
+screen.onkey(snake.down, "s")
+screen.onkey(snake.left, "a")
+screen.onkey(snake.right, "d")
+
 game_is_on = True
+
 while game_is_on:
     screen.update()
     time.sleep(0.1)
--- intermediate/day20/snake.py
+++ intermediate/day20/snake.py
@@ -1,8 +1,12 @@
-from turtle import Turtle
+from turtle import Turtle, width
 from typing import List
 
 STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
+RIGHT = 0
+UP = 90
+LEFT = 180
+DOWN = 270
 
 
 class Snake:
@@ -10,6 +14,7 @@ class Snake:
     def __init__(self) -> None:
         self.segments: List[Turtle] = []
         self.create_snake()
+        self.head = self.segments[0]
 
     def create_snake(self):
         for pos in STARTING_POSITIONS:
@@ -24,4 +29,20 @@ class Snake:
             new_x = self.segments[seg_num - 1].xcor()
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(x=new_x, y=new_y)
-        self.segments[0].forward(MOVE_DISTANCE)
+        self.head.forward(MOVE_DISTANCE)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
