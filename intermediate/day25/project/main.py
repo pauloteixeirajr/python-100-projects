3f07856f42ca39eeb2c6aa44fa5f1f493087b00a
Paulo Teixeira <pauloteixeirajr@outlook.com>
2021-04-16 23:39:34 +0100

    day25: us states game

--- intermediate/day25/project/main.py
+++ intermediate/day25/project/main.py
@@ -11,9 +11,24 @@ turtle.shape(image)
 # def get_mouse_click_coor(x, y):
 #     print(x, y)
 # turtle.onscreenclick(get_mouse_click_coor)
+states = pandas.read_csv("./intermediate/day25/project/50_states.csv")
 
-answer_state = screen.textinput(
-    title="Guess the State", prompt="What's another state's name?")
+correct_guesses = []
 
-# Keep screen open
-turtle.mainloop()
+while len(correct_guesses) < 50:
+    answer_state = screen.textinput(
+        title=f"{len(correct_guesses)}/{states.state.size} States Correct",
+        prompt="What's another state's name?"
+    ).title()
+
+    if answer_state == "Exit":
+        break
+
+    state = states[states.state == answer_state]
+    if not state.empty and answer_state not in correct_guesses:
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        t.goto(int(state.x), int(state.y))
+        t.write(answer_state)
+        correct_guesses.append(answer_state)
