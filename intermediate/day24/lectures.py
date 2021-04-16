88a429c1a57eafc29ccb75b760f415b90ab71684
Paulo Teixeira <pauloteixeirajr@outlook.com>
2021-04-16 20:58:12 +0100

    day24: understanding relative and absolute paths

--- intermediate/day24/lectures.py
+++ intermediate/day24/lectures.py
@@ -1,20 +1,25 @@
-file = open("lectures.txt")
-contents = file.read()
+# file = open("lectures.txt")
+# contents = file.read()
 
-print(contents)
-# Recommended to close the file once
-# you don't need it anymore
-file.close()
+# print(contents)
+# # Recommended to close the file once
+# # you don't need it anymore
+# file.close()
 
-# Or you can use a with statement
-# it will automatically close after
-# the with statement is complete
-with open("lectures.txt") as file:
+# # Or you can use a with statement
+# # it will automatically close after
+# # the with statement is complete
+# with open("lectures.txt") as file:
+#     contents = file.read()
+#     print(contents)
+
+# # Writing to the file
+# # w to replace all content
+# # a to append content
+# with open("lectures.txt", mode="a") as file:
+#     file.write("\nNew text.")
+
+# Understanding Relative and Absolute File Paths
+with open("./intermediate/day24/lectures.txt") as file:
     contents = file.read()
     print(contents)
-
-# Writing to the file
-# w to replace all content
-# a to append content
-with open("lectures.txt", mode="a") as file:
-    file.write("\nNew text.")
