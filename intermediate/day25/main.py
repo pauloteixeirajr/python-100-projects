61333adc43235fb68948613188bd451cc8e56a60
Paulo Teixeira <pauloteixeirajr@outlook.com>
2021-04-16 22:22:32 +0100

    day25: pandas dataframes and series

--- intermediate/day25/main.py
+++ intermediate/day25/main.py
@@ -16,4 +16,27 @@
 import pandas
 
 data = pandas.read_csv("./intermediate/day25/weather_data.csv")
-print(data["temp"])
+print(data["temp"].to_list())
+print(data["temp"].mean())
+print(data["temp"].max())
+print(data["temp"].min())
+print(data.temp)
+
+# Searching a data frame
+print(data[data.day == "Monday"])
+print(data[data.temp == data.temp.max()])
+
+monday = data[data.day == "Monday"]
+print(monday.temp, monday.condition)
+
+# Create a dataframe from scratch
+data_dict = {
+    "students": ["Amy", "James", "Agatha"],
+    "scores": [76, 56, 65]
+}
+
+new_df = pandas.DataFrame(data_dict)
+print(new_df)
+
+# You can use dataframes to save new csvs
+new_df.to_csv('./intermediate/day25/new_def.csv')
