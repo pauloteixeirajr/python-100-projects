a1ebbf485fa68d3b1fd1cf8f86c70f1ebd6b56e4
Paulo Teixeira <pauloteixeirajr@outlook.com>
2021-04-16 20:48:19 +0100

    day24: read and write the highscore to a file in Snake

--- intermediate/day24/snake-improved/scoreboard.py
+++ intermediate/day24/snake-improved/scoreboard.py
@@ -9,12 +9,20 @@ class Scoreboard(Turtle):
     def __init__(self) -> None:
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        self.load_highscore()
         self.color("white")
         self.penup()
         self.hideturtle()
         self.goto(x=0, y=270)
         self.update_scoreboard()
+
+    def load_highscore(self):
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
+
+    def save_highscore(self):
+        with open("data.txt", mode="w") as data:
+            data.write(str(self.high_score))
 
     def increase_score(self):
         self.score += 1
@@ -30,6 +38,7 @@ class Scoreboard(Turtle):
     def reset_score(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            self.save_highscore()
 
         self.score = 0
         self.update_scoreboard()
