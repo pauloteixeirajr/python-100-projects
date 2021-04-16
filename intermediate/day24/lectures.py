4d09dfd3d98548bac6df2a94cc5bcb08949b6d75
Paulo Teixeira <pauloteixeirajr@outlook.com>
2021-04-16 20:38:36 +0100

    day24: open, read write to files

---
+++ intermediate/day24/lectures.py
@@ -0,0 +1,20 @@
+file = open("lectures.txt")
+contents = file.read()
+
+print(contents)
+# Recommended to close the file once
+# you don't need it anymore
+file.close()
+
+# Or you can use a with statement
+# it will automatically close after
+# the with statement is complete
+with open("lectures.txt") as file:
+    contents = file.read()
+    print(contents)
+
+# Writing to the file
+# w to replace all content
+# a to append content
+with open("lectures.txt", mode="a") as file:
+    file.write("\nNew text.")
