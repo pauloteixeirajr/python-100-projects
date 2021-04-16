844ffdda4cca24cc04a0639755c16e0f979313e6
Paulo Teixeira <pauloteixeirajr@outlook.com>
2021-04-16 19:23:37 +0100

    day23: create the player behaviour

--- intermediate/day23/main.py
+++ intermediate/day23/main.py
@@ -8,7 +8,16 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+player = Player()
+
+screen.listen()
+screen.onkey(player.move_up, "Up")
+
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    # Check if player has reached the finish line
+    if player.has_reached_finish():
+        player.level_up()
--- intermediate/day23/player.py
+++ intermediate/day23/player.py
@@ -1,7 +1,25 @@
+from turtle import Turtle
+
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
-class Player:
-    pass
+class Player(Turtle):
+
+    def __init__(self) -> None:
+        super().__init__(shape="turtle")
+        self.penup()
+        self.setheading(90)
+        self.goto(STARTING_POSITION)
+        self.level = 1
+
+    def move_up(self):
+        self.forward(MOVE_DISTANCE)
+
+    def level_up(self):
+        self.level += 1
+        self.goto(STARTING_POSITION)
+
+    def has_reached_finish(self):
+        return self.ycor() > FINISH_LINE_Y
