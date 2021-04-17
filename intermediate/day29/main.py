8964346588f104acc3e053c23eba5977601bc2b7
Paulo Teixeira <pauloteixeirajr@outlook.com>
2021-04-17 21:55:05 +0100

    day29: creating the user interface

--- intermediate/day29/main.py
+++ intermediate/day29/main.py
@@ -1,5 +1,40 @@
+from tkinter import *
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Password Manager")
+window.config(padx=50, pady=50)
+
+canvas = Canvas(width=200, height=200)
+logo_img = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=logo_img)
+canvas.grid(row=0, column=1)
+
+website_lbl = Label(text="Website: ")
+website_lbl.grid(row=1, column=0)
+
+website_entry = Entry(width=35)
+website_entry.grid(row=1, column=1, columnspan=2)
+
+email_lbl = Label(text="Email/Username: ")
+email_lbl.grid(row=2, column=0)
+
+email_entry = Entry(width=35)
+email_entry.grid(row=2, column=1, columnspan=2)
+
+password_lbl = Label(text="Password: ")
+password_lbl.grid(row=3, column=0)
+
+password_entry = Entry(width=21)
+password_entry.grid(row=3, column=1)
+
+password_btn = Button(text="Generate Password")
+password_btn.grid(row=3, column=2)
+
+add_btn = Button(text="Add", width=36)
+add_btn.grid(row=4, column=1, columnspan=2)
+
+window.mainloop()
