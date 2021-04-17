ab295e434b68b7e2849e2e44f42f981a5af42433
Paulo Teixeira <pauloteixeirajr@outlook.com>
2021-04-17 20:19:15 +0100

    day28: add a count down mechanism

--- intermediate/day28/main.py
+++ intermediate/day28/main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -13,7 +14,26 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 
+
+def start_timer():
+    count_down(5 * 60)
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+
+
+def count_down(count):
+    min = math.floor(count / 60)
+    sec = count % 60
+
+    if min < 10:
+        min = f"0{min}"
+
+    if sec < 10:
+        sec = f"0{sec}"
+
+    canvas.itemconfig(timer_text, text=f"{min}:{sec}")
+    if count > 0:
+        window.after(1000, count_down, count - 1)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -26,13 +46,13 @@ title_lbl.grid(row=0, column=1)
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(
+timer_text = canvas.create_text(
     100, 130, text="00:00", fill="#fff",
     font=(FONT_NAME, 35, "bold")
 )
 canvas.grid(row=1, column=1)
 
-start_btn = Button(text="Start", highlightthickness=0)
+start_btn = Button(text="Start", highlightthickness=0, command=start_timer)
 start_btn.grid(row=2, column=0)
 
 reset_btn = Button(text="Reset", highlightthickness=0)
