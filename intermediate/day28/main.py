68ed415bc3e636dae8c78ff79f65eae0bd836aba
Paulo Teixeira <pauloteixeirajr@outlook.com>
2021-04-17 19:47:26 +0100

    day28: complete application UI

--- intermediate/day28/main.py
+++ intermediate/day28/main.py
@@ -20,6 +20,9 @@ window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
+title_lbl = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50))
+title_lbl.grid(row=0, column=1)
+
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
@@ -27,7 +30,16 @@ canvas.create_text(
     100, 130, text="00:00", fill="#fff",
     font=(FONT_NAME, 35, "bold")
 )
-canvas.pack()
+canvas.grid(row=1, column=1)
+
+start_btn = Button(text="Start", highlightthickness=0)
+start_btn.grid(row=2, column=0)
+
+reset_btn = Button(text="Reset", highlightthickness=0)
+reset_btn.grid(row=2, column=2)
+
+checkmark = Label(text="✔", fg=GREEN, bg=YELLOW)
+checkmark.grid(row=3, column=1)
 
 # Keep the screen open
 window.mainloop()
