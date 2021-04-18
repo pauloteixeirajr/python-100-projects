971fbead72f16f4431af98ffccb617a2d70f7f97
Paulo Teixeira <pauloteixeirajr@outlook.com>
2021-04-18 14:45:35 +0100

    day31: create new flash cards on btn click

--- intermediate/day31/main.py
+++ intermediate/day31/main.py
@@ -1,8 +1,20 @@
+import pandas
+import random
 from tkinter import *
 
 BACKGROUND_COLOR = "#B1DDC6"
 TITLE_FONT = ("Arial", 40, "italic")
 WORD_FONT = ("Arial", 60, "bold")
+WORDS_DF = pandas.read_csv("./data/french_words.csv")
+WORDS_LIST = WORDS_DF.to_dict(orient="records")
+
+
+# Get Random French Word
+def next_card():
+    random_card = random.choice(WORDS_LIST)
+    canvas.itemconfig(title_text, text="French")
+    canvas.itemconfig(word_text, text=random_card["French"])
+
 
 window = Tk()
 window.title("Flashy")
@@ -12,16 +24,25 @@ canvas = Canvas(width=800, height=526)
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 card_front_img = PhotoImage(file="./images/card_front.png")
 canvas.create_image(400, 263, image=card_front_img)
-canvas.create_text(400, 150, text="Title", font=TITLE_FONT)
-canvas.create_text(400, 263, text="Word", font=WORD_FONT)
+title_text = canvas.create_text(400, 150, text="", font=TITLE_FONT)
+word_text = canvas.create_text(400, 263, text="", font=WORD_FONT)
+
 canvas.grid(row=0, column=0, columnspan=2)
 
 wrong_image = PhotoImage(file="./images/wrong.png")
-wrong_button = Button(image=wrong_image, highlightthickness=0)
+wrong_button = Button(
+    image=wrong_image,
+    highlightthickness=0,
+    command=next_card)
 wrong_button.grid(row=1, column=0)
 
 right_image = PhotoImage(file="./images/right.png")
-right_button = Button(image=right_image, highlightthickness=0)
+right_button = Button(
+    image=right_image,
+    highlightthickness=0,
+    command=next_card)
 right_button.grid(row=1, column=1)
+
+next_card()
 
 window.mainloop()
