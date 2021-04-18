93eba8c1c91927998d457ca847e8edc435b31c98
Paulo Teixeira <pauloteixeirajr@outlook.com>
2021-04-18 12:47:58 +0100

    day30: catching exceptions with try catch

---
+++ intermediate/day30/lectures.py
@@ -0,0 +1,13 @@
+# Catching Expressions: The try catch except finally Pattern
+try:
+    file = open("a_file.txt")
+except FileNotFoundError:
+    file = open("a_file.txt", "w")
+    file.write("Something")
+except KeyError as error_message:
+    print(f"The key {error_message} does not exist.")
+else:
+    content = file.read()
+    print(content)
+finally:
+    file.close()
