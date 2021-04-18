aecb09651b6aed7069c8bb2f887109a491a32af4
Paulo Teixeira <pauloteixeirajr@outlook.com>
2021-04-18 14:32:45 +0100

    day31: create the user interface

--- intermediate/day31/main.py
+++ intermediate/day31/main.py
@@ -1,1 +1,27 @@
+from tkinter import *
+
 BACKGROUND_COLOR = "#B1DDC6"
+TITLE_FONT = ("Arial", 40, "italic")
+WORD_FONT = ("Arial", 60, "bold")
+
+window = Tk()
+window.title("Flashy")
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+canvas = Canvas(width=800, height=526)
+canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+card_front_img = PhotoImage(file="./images/card_front.png")
+canvas.create_image(400, 263, image=card_front_img)
+canvas.create_text(400, 150, text="Title", font=TITLE_FONT)
+canvas.create_text(400, 263, text="Word", font=WORD_FONT)
+canvas.grid(row=0, column=0, columnspan=2)
+
+wrong_image = PhotoImage(file="./images/wrong.png")
+wrong_button = Button(image=wrong_image, highlightthickness=0)
+wrong_button.grid(row=1, column=0)
+
+right_image = PhotoImage(file="./images/right.png")
+right_button = Button(image=right_image, highlightthickness=0)
+right_button.grid(row=1, column=1)
+
+window.mainloop()
