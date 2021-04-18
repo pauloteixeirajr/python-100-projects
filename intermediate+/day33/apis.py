cf21bdb6fb4844487eff8a938cd0387e4cac23fa
Paulo Teixeira <pauloteixeirajr@outlook.com>
2021-04-18 18:43:25 +0100

    day33: using api parameters

--- intermediate+/day33/apis.py
+++ intermediate+/day33/apis.py
@@ -1,13 +1,36 @@
 # Application Programming Interfaces (APIs)
 import requests
+from datetime import datetime
 
 
-endpoint = "http://api.open-notify.org/iss-now.json"
-response = requests.get(url=endpoint)
+iss_endpoint = "http://api.open-notify.org/iss-now.json"
+response = requests.get(url=iss_endpoint)
 response.raise_for_status()
 
 data = response.json()
-lon = data["iss_position"]["longitude"]
+lng = data["iss_position"]["longitude"]
 lat = data["iss_position"]["latitude"]
 
-print(lat, lon)
+print(lat, lng)
+
+# Understand API Parameters
+params = {
+    "lat": lat,
+    "lng": lng,
+    "formatted": 0
+}
+
+sunset_endpoint = "https://api.sunrise-sunset.org/json"
+response = requests.get(sunset_endpoint, params=params)
+response.raise_for_status()
+results = response.json()["results"]
+sunrise = results["sunrise"]
+sunset = results["sunset"]
+
+sunrise_hour = sunrise.split("T")[1].split(":")[0]
+sunset_hour = sunset.split("T")[1].split(":")[0]
+
+time_now = datetime.now()
+
+print(sunrise_hour, sunset_hour)
+print(time_now.hour)
