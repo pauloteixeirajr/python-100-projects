f65cbdca326e978da27afe4ca8a854ed2120357e
Paulo Teixeira <pauloteixeirajr@outlook.com>
2021-04-18 12:51:18 +0100

    day30: raising your own exceptions

--- intermediate/day30/lectures.py
+++ intermediate/day30/lectures.py
@@ -1,13 +1,23 @@
 # Catching Expressions: The try catch except finally Pattern
-try:
-    file = open("a_file.txt")
-except FileNotFoundError:
-    file = open("a_file.txt", "w")
-    file.write("Something")
-except KeyError as error_message:
-    print(f"The key {error_message} does not exist.")
-else:
-    content = file.read()
-    print(content)
-finally:
-    file.close()
+# try:
+#     file = open("a_file.txt")
+# except FileNotFoundError:
+#     file = open("a_file.txt", "w")
+#     file.write("Something")
+# except KeyError as error_message:
+#     print(f"The key {error_message} does not exist.")
+# else:
+#     content = file.read()
+#     print(content)
+# finally:
+#     file.close()
+
+# Raising your own exceptions
+height = float(input("Height: "))
+weight = int(input("Weight: "))
+
+if height > 3:
+    raise ValueError("Height should be less than 3 meters")
+
+bmi = weight / height ** 2
+print(bmi)
