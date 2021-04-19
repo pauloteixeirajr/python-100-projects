1a23e527467ac875164775b11332a9cff84772f0
Paulo Teixeira <pauloteixeirajr@outlook.com>
2021-04-19 20:53:02 +0100

    day35: sending SMS via Twilio API

--- intermediate+/day35/main.py
+++ intermediate+/day35/main.py
@@ -1,7 +1,10 @@
 import requests
+from twilio.rest import Client
 
 api_key = "API_KEY"
 endpoint = "https://api.openweathermap.org/data/2.5/onecall"
+account_sid = "ACCOUNT_SID"
+auth_token = "AUTH_TOKEN"
 
 params = {
     "lat": 54.787715,
@@ -17,8 +20,19 @@ response.raise_for_status()
 data = response.json()
 next_12_hours = data["hourly"][:12]
 
+will_rain = False
+
 for hour in next_12_hours:
     for weather in hour["weather"]:
-        print(weather["main"])
         if weather["id"] < 700:
-            print("Bring an umbrella")
+            will_rain = True
+
+if will_rain:
+    client = Client(account_sid, auth_token)
+    message = client.messages \
+        .create(
+            body="It's going to rain today. Remember to bring an 🌂",
+            from_="FROM_NUMBER",
+            to="TO_NUMBER"
+        )
+    print(message.status)
