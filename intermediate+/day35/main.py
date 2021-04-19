90866d85e899ad990b863ed6682ea2aa885da234
Paulo Teixeira <pauloteixeirajr@outlook.com>
2021-04-19 21:14:15 +0100

    day35: using environment variables and hiding API keys

--- intermediate+/day35/main.py
+++ intermediate+/day35/main.py
@@ -1,10 +1,11 @@
+import os
 import requests
 from twilio.rest import Client
 
-api_key = "API_KEY"
+api_key = os.environ.get("OWM_API_KEY")
 endpoint = "https://api.openweathermap.org/data/2.5/onecall"
-account_sid = "ACCOUNT_SID"
-auth_token = "AUTH_TOKEN"
+account_sid = os.environ.get("TWILIO_ACCOUNT_SID")
+auth_token = os.environ.get("TWILIO_AUTH_TOKEN")
 
 params = {
     "lat": 54.787715,
@@ -32,7 +33,7 @@ if will_rain:
     message = client.messages \
         .create(
             body="It's going to rain today. Remember to bring an 🌂",
-            from_="FROM_NUMBER",
-            to="TO_NUMBER"
+            from_=os.environ.get("TWILIO_FROM_NUMBER"),
+            to=os.environ.get("TWILIO_TO_NUMBER")
         )
     print(message.status)
