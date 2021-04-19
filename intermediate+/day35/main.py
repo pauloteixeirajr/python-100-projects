effd0ffaeab829ce25637a1f0bbd5c8144acd74f
Paulo Teixeira <pauloteixeirajr@outlook.com>
2021-04-19 20:39:47 +0100

    day35: check if it will rain in the next 12 hours

--- intermediate+/day35/main.py
+++ intermediate+/day35/main.py
@@ -4,14 +4,21 @@ api_key = "API_KEY"
 endpoint = "https://api.openweathermap.org/data/2.5/onecall"
 
 params = {
-    "lat": 53.34807,
-    "lon": -6.24827,
+    "lat": 54.787715,
+    "lon": -6.492315,
     "appid": api_key,
     "units": "metric",
+    "exclude": "current,minutely,daily"
 }
 
 response = requests.get(endpoint, params=params)
 response.raise_for_status()
 
-print(response.status_code)
-print(response.json())
+data = response.json()
+next_12_hours = data["hourly"][:12]
+
+for hour in next_12_hours:
+    for weather in hour["weather"]:
+        print(weather["main"])
+        if weather["id"] < 700:
+            print("Bring an umbrella")
