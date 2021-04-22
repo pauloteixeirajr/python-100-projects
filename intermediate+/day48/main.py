5daa15758153647bb705b378e384967fccd91c7b
Paulo Teixeira <pauloteixeirajr@outlook.com>
2021-04-22 22:29:45 +0100

    day48: scrape data with selenium

--- intermediate+/day48/main.py
+++ intermediate+/day48/main.py
@@ -4,13 +4,24 @@ chrome_driver_path = "/Users/paulot/Code/chromedriver"
 driver = webdriver.Chrome(chrome_driver_path)
 
 driver.get("https://python.org")
-# price = driver.find_element_by_id("priceblock_ourprice")
-# driver.find_element_by_name("query")
-# driver.find_element_by_class_name("buttons")
-# driver.find_elements_by_css_selector(".documentation a")
-anchor = driver.find_element_by_xpath(
-    '//*[@id="site-map"]/div[2]/div/ul/li[3]/a')
+events_list = driver.find_elements_by_css_selector(".event-widget li")
+events = {}
 
-print(anchor.text)
+for i, event in enumerate(events_list):
+    time, name = event.text.split("\n")
+    events[i] = {
+        "time": time,
+        "name": name,
+    }
+
+print(events)
+# # price = driver.find_element_by_id("priceblock_ourprice")
+# # driver.find_element_by_name("query")
+# # driver.find_element_by_class_name("buttons")
+# # driver.find_elements_by_css_selector(".documentation a")
+# anchor = driver.find_element_by_xpath(
+#     '//*[@id="site-map"]/div[2]/div/ul/li[3]/a')
+
+# print(anchor.text)
 
 driver.quit()
