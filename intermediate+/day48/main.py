b2452bfa2d1fbbdd42bf7cb2a2762ded71b0e5a6
Paulo Teixeira <pauloteixeirajr@outlook.com>
2021-04-22 22:01:15 +0100

    day48: find and select elements with selenium

--- intermediate+/day48/main.py
+++ intermediate+/day48/main.py
@@ -3,6 +3,14 @@ from selenium import webdriver
 chrome_driver_path = "/Users/paulot/Code/chromedriver"
 driver = webdriver.Chrome(chrome_driver_path)
 
-driver.get("https://www.amazon.co.uk")
+driver.get("https://python.org")
+# price = driver.find_element_by_id("priceblock_ourprice")
+# driver.find_element_by_name("query")
+# driver.find_element_by_class_name("buttons")
+# driver.find_elements_by_css_selector(".documentation a")
+anchor = driver.find_element_by_xpath(
+    '//*[@id="site-map"]/div[2]/div/ul/li[3]/a')
+
+print(anchor.text)
 
 driver.quit()
