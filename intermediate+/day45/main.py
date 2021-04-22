ac736cae257917502170aa3502943681a1a601ed
Paulo Teixeira <pauloteixeirajr@outlook.com>
2021-04-22 19:39:07 +0100

    day45: finding and selecting elements with beautifulsoup

--- intermediate+/day45/main.py
+++ intermediate+/day45/main.py
@@ -1,3 +1,4 @@
+from os import name
 from bs4 import BeautifulSoup
 
 with open('website.html') as html:
@@ -7,4 +8,23 @@ soup = BeautifulSoup(markup, 'html.parser')
 print(soup.title)
 # print(soup.body)
 
-print(soup.a)
+all_anchors = soup.find_all(name="a")
+for tag in all_anchors:
+    # Get inner text
+    print(tag.getText())
+    # Get attributes
+    print(tag.get("href"))
+
+# Finding by ID
+heading = soup.find(name="h1", id="name")
+# heading = soup.select_one("#name")
+print(heading.getText())
+
+# Finding by class
+section = soup.find(name="h3", class_="heading")
+# section = soup.select(".heading")
+print(section.getText())
+
+# Finding nested children
+first_anchor = soup.select_one("p a")
+print(first_anchor.get("href"))
