434088b599d9cd2c7dbd215ba1411319d94ced48
Paulo Teixeira <pauloteixeirajr@outlook.com>
2021-04-23 16:40:23 +0100

    day55: advanced decorators with *args and **kwargs

--- intermediate+/day55/main.py
+++ intermediate+/day55/main.py
@@ -22,5 +22,26 @@ def greet(name, number):
     return f"Hello, {name}. You are number {number + 1}"
 
 
+class User:
+    def __init__(self, name) -> None:
+        self.name = name
+        self.is_logged_in = False
+
+
+def is_authenticated(function):
+    def wrapper(*args, **kwargs):
+        if args[0].is_logged_in:
+            function(args[0])
+    return wrapper
+
+
+@is_authenticated
+def create_blog_post(user):
+    print(f"This is {user.name}'s new blog post.")
+
+
+new_user = User("Paulo")
+create_blog_post(new_user)
+
 if __name__ == '__main__':
     app.run(debug=True)
