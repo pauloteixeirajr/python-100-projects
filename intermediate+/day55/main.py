01ca79db70bba735d7bb94a94187582c1b1f2cad
Paulo Teixeira <pauloteixeirajr@outlook.com>
2021-04-23 16:25:07 +0100

    day55: rendering html elements

--- intermediate+/day55/main.py
+++ intermediate+/day55/main.py
@@ -3,6 +3,11 @@ from flask import Flask
 app = Flask(__name__)
 
 
+@app.route("/")
+def hello_world():
+    return "<h1>Hello, World!</h1>"
+
+
 @app.route("/<name>/<int:number>")
 def greet(name, number):
     return f"Hello, {name}. You are number {number + 1}"
