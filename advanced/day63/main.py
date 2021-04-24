f1d669fa26cadae642ba735602402a494b863226
Paulo Teixeira <pauloteixeirajr@outlook.com>
2021-04-24 17:12:34 +0100

    day63: build a sqlite database into the flask webiste with sqlachemy

--- advanced/day63/main.py
+++ advanced/day63/main.py
@@ -1,29 +1,69 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///new-books-collection.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
 
-all_books = []
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+
+    # Optional: this will allow each book object to be identified by its title when printed.
+    def __repr__(self):
+        return f'<Book {self.title}>'
+
+# db.create_all() Only needs to run once to create the DB
 
 
 @app.route('/')
 def home():
+    all_books = db.session.query(Book).all()
     return render_template("index.html", books=all_books)
 
 
 @app.route("/add", methods=["GET", "POST"])
 def add():
     if request.method == "POST":
-        book = {
-            "title": request.form.get("title"),
-            "author": request.form.get("author"),
-            "rating": request.form.get("rating"),
-        }
 
-        all_books.append(book)
+        book = Book(
+            title=request.form.get("title"),
+            author=request.form.get("author"),
+            rating=request.form.get("rating")
+        )
+
+        db.session.add(book)
+        db.session.commit()
 
         return redirect(url_for("home"))
 
     return render_template("add.html")
+
+
+@app.route("/edit", methods=["GET", "POST"])
+def edit():
+    book_id = request.args.get("id")
+    book = Book.query.get(book_id)
+    if request.method == "POST":
+        book.rating = request.form.get("rating")
+        db.session.commit()
+
+        return redirect(url_for("home"))
+    return render_template("rating.html", book=book)
+
+
+@app.route("/delete", methods=["GET"])
+def delete():
+    book_id = request.args.get("id")
+    book = Book.query.get(book_id)
+    db.session.delete(book)
+    db.session.commit()
+
+    return redirect(url_for("home"))
 
 
 if __name__ == "__main__":
