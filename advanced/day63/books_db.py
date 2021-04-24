6489029979828132e781d91ca914fa19a54008eb
Paulo Teixeira <pauloteixeirajr@outlook.com>
2021-04-24 16:26:22 +0100

    day63: sqlite databases

---
+++ advanced/day63/books_db.py
@@ -0,0 +1,14 @@
+import sqlite3
+
+db = sqlite3.connect("books-collection.db")
+cursor = db.cursor()
+
+# Create the books table
+cursor.execute("""
+    CREATE TABLE
+        books (
+            id INTEGER PRIMARY KEY,
+            title varchar(250) NOT NULL UNIQUE,
+            author varchar(250) NOT NULL,
+            rating FLOAT NOT NULL
+        )""")
